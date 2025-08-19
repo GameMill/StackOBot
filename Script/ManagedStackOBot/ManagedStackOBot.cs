e634b0f5a17842d66dd9e8f2962817c0e0541fc0
Christopher Boardman <89698445+GameMill@users.noreply.github.com>
2025-08-19 18:48:09 +0100

    CleanProjectWithU#

---
+++ Script/ManagedStackOBot/ManagedStackOBot.cs
@@ -0,0 +1,16 @@
+using UnrealSharp.Engine.Core.Modules;
+
+namespace ManagedStackOBot;
+
+public class FManagedStackOBot : IModuleInterface
+{
+    public void StartupModule()
+    {
+
+    }
+
+    public void ShutdownModule()
+    {
+
+    }
+}
